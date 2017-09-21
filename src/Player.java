9df7871fc9009ce66b5a6369e80481d51ec30c1a
Robert <robbanl@kth.se>
2017-09-21 14:51:54 +0200

    minimax

--- src/Player.java
+++ src/Player.java
@@ -13,7 +13,6 @@ public class Player {
     public GameState play(final GameState gameState, final Deadline deadline) {
         Vector<GameState> nextStates = new Vector<GameState>();
         gameState.findPossibleMoves(nextStates);
-
         if (nextStates.size() == 0) {
             // Must play "pass" move if there are no other moves possible.
             return new GameState(gameState, new Move());
@@ -25,5 +24,59 @@ public class Player {
          */
         Random random = new Random();
         return nextStates.elementAt(random.nextInt(nextStates.size()));
-    }    
+    }
+
+    GameState minimax(GameState state) {
+        Vector<GameState> nextStates = new Vector<GameState>();
+        state.findPossibleMoves(nextStates);
+
+        GameState s = state;
+        int max = -1;
+        for (GameState nextState : nextStates) {
+            int v = minsearch(nextState, Integer.MIN_VALUE, Integer.MAX_VALUE);
+            if (v > max) {
+                max = v;
+                s = nextState;
+            }
+        }
+        return s;
+    }
+
+    int maxsearch(GameState state, int alpha, int beta) {
+        Vector<GameState> nextStates = new Vector<GameState>();
+        state.findPossibleMoves(nextStates);
+
+        if (state.isXWin())
+            return 1;
+        else if (state.isOWin())
+            return 0;
+
+
+
+        return 1;
+    }
+
+    int minsearch(GameState state, int alpha, int beta) {
+        Vector<GameState> nextStates = new Vector<GameState>();
+        state.findPossibleMoves(nextStates);
+
+        if (state.isXWin())
+            return 1;
+        else if (state.isOWin())
+            return 0;
+
+        for (GameState nextState : nextStates) {
+            int v = maxsearch(nextState, alpha, beta);
+            if (v < beta) {
+                beta = v;
+            }
+            if (v < alpha)
+                break;
+        }
+        return beta;
+    }
+
+    int utility(GameState state) {
+
+    }
 }
